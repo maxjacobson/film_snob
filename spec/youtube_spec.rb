4d8db59b518810ff4af1db0c4438ecac1b5f1465
Max Jacobson <max@hardscrabble.net>
2014-04-19 19:03:33 -0400

    fix an outdated spec

--- spec/youtube_spec.rb
+++ spec/youtube_spec.rb
@@ -4,8 +4,7 @@ describe FilmSnob::YouTube do
   it 'may not be embeddable' do
     VCR.use_cassette('bad_youtube_url') do
       snob = FilmSnob.new('http://youtube.com/watch?v=malformedid')
-      video = snob.video
-      expect{video.html}.to raise_error(FilmSnob::NotEmbeddableError)
+      expect{snob.html}.to raise_error(FilmSnob::NotEmbeddableError)
     end
   end
 end
