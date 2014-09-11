4f5cf6b3ba5336613962a362ce33b35607c079a0
Will Lowry <william.c.lowry@gmail.com>
2014-09-10 22:37:59 -0400

    pass parsing url test

--- lib/film_snob/rdio.rb
+++ lib/film_snob/rdio.rb
@@ -11,13 +11,13 @@ class FilmSnob
       ]
     end
 
-    # def clean_url
-    #   @clean_url ||= "https://www.dailymotion.com/video/#{id}"
-    # end
+    def clean_url
+      @clean_url ||= url 
+    end
 
-    # def self.oembed_endpoint
-    #   'http://www.dailymotion.com/services/oembed'
-    # end
+    def self.oembed_endpoint
+      'http://www.rdio.com/api/oembed/'
+    end
 
   end
 end
