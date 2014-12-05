c15965e261757cc5d45ead3f54853e331b6771c1
Wil Chandler <wilchandler2@gmail.com>
2014-12-05 12:07:27 -0600

    Added deprecation alias/warning for #watchable (now #embeddable)

--- lib/film_snob.rb
+++ lib/film_snob.rb
@@ -1,18 +1,24 @@
 require "film_snob/version"
 require "film_snob/url_to_video"
 require "film_snob/exceptions"
+require "film_snob/deprecated"
 
 class FilmSnob
   attr_reader :url
+
+  extend Deprecated
 
   def initialize(url, options = {})
     @url = url
     @video = UrlToVideo.new(url, options).video
   end
 
-  def watchable?
+  def embeddable?
     !@video.nil?
   end
+
+  deprecated_alias :watchable?, :embeddable?, \
+    "#watchable will be deprecated as of v1.0."
 
   def method_missing(message)
     if delegated_video_methods.include?(message)
