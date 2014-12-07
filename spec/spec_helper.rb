23fea87b34b19c7714c4ed220795e96c320b833f
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2014-12-07 07:29:07 +0530

    Fixing rubocop issues.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,10 +40,10 @@ RSpec.configure do |config|
   #   # get run.
   #   config.filter_run :focus
   #   config.run_all_when_everything_filtered = true
-
-  # Many RSpec users commonly either run the entire suite or an individual
-  # file, and it's useful to allow more verbose output when running an
-  # individual spec file.
+  #
+  #   # Many RSpec users commonly either run the entire suite or an individual
+  #   # file, and it's useful to allow more verbose output when running an
+  #   # individual spec file.
   if config.files_to_run.one?
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
@@ -86,18 +86,18 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  #   // https://github.com/rspec/rspec-core/issues/1571
-  #   config.warnings = true
+  # // https://github.com/rspec/rspec-core/issues/1571
+  # config.warnings = true
   config.raise_errors_for_deprecations!
-  #
-  #   # rspec-mocks config goes here. You can use an alternate test double
-  #   # library (such as bogus or mocha) by changing the `mock_with` option here.
+
+  # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
     # Enable only the newer, non-monkey-patching expect syntax.
     # For more details, see:
     #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
     # mocks.syntax = :expect
-    #
+
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended.
     mocks.verify_partial_doubles = true
