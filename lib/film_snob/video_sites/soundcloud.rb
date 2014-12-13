62958779055f7774cd90b7a4251bec27b98ee7f4
Denise <yu.denise.d@gmail.com>
2014-12-13 12:29:20 +0000

    Soundcloud Integration
    Passing in standard Soundcloud urls

---
+++ lib/film_snob/video_sites/soundcloud.rb
@@ -0,0 +1,26 @@
+require "film_snob/video_site"
+
+class FilmSnob
+
+  class Soundcloud < VideoSite
+
+    def self.valid_url_patterns
+      [ %r{https://soundcloud.com/(.+)} ]
+    end
+
+    def self.oembed_endpoint
+      "https://soundcloud.com/oembed"
+    end
+
+    def clean_url
+      @clean_url ||= "https://soundcloud.com/#{id}"
+    end
+
+    def options
+      {format: 'json'}.merge(@options)
+    end
+
+  end
+
+end
+
---
+++ spec/film_snob/video_sites/soundcloud_spec.rb
@@ -0,0 +1,13 @@
+describe FilmSnob::Soundcloud do 
+
+  it "should parse normal Soundcloud URLs" do
+    snob = FilmSnob.new("https://soundcloud.com/david_rees/we-are-never-ever-getting-girl-boygether")
+    expect(snob).to be_embeddable
+    expect(snob.id).to eq "david_rees/we-are-never-ever-getting-girl-boygether"
+    expect(snob.site).to eq :soundcloud
+    VCR.use_cassette("soundcloud/taylor-swift") do
+      expect(snob.title).to eq "APHEX SWIFT: We Are Never Ever Getting Girl/Boygether by David_Rees"
+    end
+  end
+
+end
