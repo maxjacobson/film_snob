bb9f1fa658afcd0f02b0708a58bc1a5de9806807
Max Jacobson <max@hardscrabble.net>
2016-05-28 15:28:34 -0400

    Don't install guard and pry on CI
    There are some errors because older versions of Ruby aren't supported,
    and they're not really needed anyway.

--- gems.rb
+++ gems.rb
@@ -2,7 +2,6 @@ source "https://rubygems.org"
 
 gem "rake", "~> 10.4"
 
-gem "pry", "~> 0.10"
 gem "rubocop", "~> 0.39"
 gem "todo_lint", "~> 0.2"
 gem "rspec", "~> 3.4"
@@ -10,10 +9,15 @@ gem "webmock", "~> 1.22"
 gem "vcr", "~> 2.9"
 gem "codeclimate-test-reporter", "~> 0.4"
 
-# TDD workflow
-gem "guard"
-gem "guard-rspec"
-gem "guard-rubocop"
+# to be skipped on CI
+group :development do
+  # TDD workflow
+  gem "guard"
+  gem "guard-rspec"
+  gem "guard-rubocop"
+
+  gem "pry", "~> 0.10"
+end
 
 # Specify your gem's dependencies in film_snob.gemspec
 gemspec
