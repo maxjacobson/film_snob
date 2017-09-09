2a4fc217cf702dff6671160ecf8f46cb4941d43f
Max Jacobson <max@hardscrabble.net>
2017-09-09 19:19:34 -0400

    Unlock some test dependencies to fix weird error

--- gems.rb
+++ gems.rb
@@ -6,8 +6,8 @@ gem "codeclimate-test-reporter", "~> 0.4"
 gem "rspec", "~> 3.4"
 gem "rubocop", "~> 0.47.1"
 gem "todo_lint", "~> 0.2"
-gem "vcr", "~> 2.9"
-gem "webmock", "~> 1.22"
+gem "vcr"
+gem "webmock"
 
 # to be skipped on CI
 group :development do
